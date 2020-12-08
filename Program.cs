29afbe192d06a094ba1fe5164c75ede27c459c86
BrunoF-M <bruno.freitas.marmol77.bm@gmail.com>
2020-12-08 05:48:46 -0300

    Versão pré final do exercício

--- Program.cs
+++ Program.cs
@@ -8,85 +8,73 @@ namespace Media4Notas
         {
             Console.Clear();
 
-            string not1;
-            Int32 note1;
-            bool nota1;
+            double not1;
+            double not2;
+            double not3;
+            double not4;
 
-            int not2;
-            double note2;
-            bool nota2;
-
-            string not3;
-            int note3;
-            bool nota3;
-
-            string not4;
-            int note4;
-            bool nota4;
-
-            Console.ForegroundColor=ConsoleColor.DarkGreen;
-            Console.WriteLine("+=+=+ Media +=+=+\n");
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("\n+=-= Média =-=+\n");
             Console.ResetColor();
 
-            Console.WriteLine("Digite suas notas abaixo.\n");
-            
+            Console.WriteLine("Digite as suas notas abaixo.");
+
             Console.Write("Nota 1: ");
-            not1 = Console.ReadLine();
-
-            nota1 = Int32.TryParse(not1, out note1);
-
+            not1 = double.Parse(Console.ReadLine());
+            
             Console.Write("Nota 2: ");
-            not2 = Int32.Parse(Console.ReadLine());
-
-            nota2 = double.TryParse(not2, out note2);
+            not2 = double.Parse(Console.ReadLine());
 
             Console.Write("Nota 3: ");
-            not3 = Console.ReadLine();
-
-            nota3 = Int32.TryParse(not3, out note3);
+            not3 = double.Parse(Console.ReadLine());
 
             Console.Write("Nota 4: ");
-            not4 = Console.ReadLine(); 
-           
-            nota4 = Int32.TryParse(not4, out note4);
+            not4 = double.Parse(Console.ReadLine());
 
-
-            if(!nota1) //se for negativo faz o 1 bloco se for positivo faz o 2 bloco
+            if (not1 < 0 || not1 > 10)
             {
-                Console.WriteLine("\nDigite somente notas entre 0 e 10.");
+                Console.WriteLine("\nDigite somente notas entre 0 e 10.\n");
                 Environment.Exit(-1);
             }
-            else
+            if (not2 < 0 || not2 > 10)
             {
-                if(!nota2)
-                {
-                    Console.WriteLine("\nDigite somente notas entre 0 e 10.");
-                    Environment.Exit(-1);
-
-                }
-                else
-                {
-                    if(!nota3)
-                    {
-                    Console.WriteLine("\nDigite somente notas entre 0 e 10.");
-                    Environment.Exit(-1);
-                    }
-                    else
-                    {
-                        if(!nota4)
-                        {
-                            Console.WriteLine("\nDigite somente notas entre 0 e 10.");
-                            Environment.Exit(-1);
-                        }
-                        else
-                        {
-                           decimal resultado = (note1 + note2 + note3 + note4 /7);
-
-                           Console.WriteLine($"seu resultado é {resultado}.");
-                        }
-                    }
-                }
+                Console.WriteLine("\nDigite somente notas entre 0 e 10.\n");
+                Environment.Exit(-1);
             }
+            if (not3 < 0 || not3 > 10)
+            {
+                Console.WriteLine("\nDigite somente notas entre 0 e 10.\n");
+                Environment.Exit(-1);
+            }
+            if (not4 < 0 || not4 > 10)
+            {
+                Console.WriteLine("\nDigite somente notas entre 0 e 10.\n");
+                Environment.Exit(-1);
+            }
+            double result;
+            double resultfinal;
+            
+            result = (not1 + not2 + not3 + not4);
+            resultfinal = (result /4);
+
+
+            if (resultfinal >= 5 || resultfinal >= 6 )
+            {
+                Console.WriteLine($"\nVocê ficou com média {resultfinal} Resultado: Em recuperação");
+            }
+            if (resultfinal > 4 )
+            {
+                Console.WriteLine($"\nVocê ficou com média {resultfinal} Resultado: Reprovado");
+            }
+            if (resultfinal < 6 )
+            {
+                Console.WriteLine($"\nVocê ficou com média {resultfinal} Resultado: Aprovado");
+            }
+
+            Console.WriteLine();
+            Console.Write("Orbigado por usar e até o ano que vem ;)... ");
+            Console.ReadKey();
+            Console.Clear();
         }
     }
 }
